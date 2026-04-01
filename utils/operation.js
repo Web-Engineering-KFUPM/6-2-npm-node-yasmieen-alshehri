b85f8bd71ae7cd131ed1876425ec27c5bef26d87
Yasmeen Alshehri <s202271660@kfupm.edu.sa>
2026-04-01 11:27:02 +0300

    TODO 4.2

--- utils/operation.js
+++ utils/operation.js
@@ -3,3 +3,8 @@ export function add(numbers) {
   return numbers.reduce((sum, num) => sum + num, 0);
 }
 
+// #TODO 4.2: Subtract function
+export function subtract(numbers) {
+  return numbers.slice(1).reduce((result, num) => result - num, numbers[0]);
+}
+
